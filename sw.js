af6b8c239a2288aa07e93bede25f0ec23050604f
gahissmk <gahisse822@gmail.com>
2026-01-15 21:37:07 +0100

    aa

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.open('neo').then(cache =>
       cache.match(e.request).then(response =>
-        response || fetch(e.request) // Si pas trouvé, fetch normalement
+        response || fetch(e.request)
       )
     ).catch(() => new Response("Fichier introuvable", {status:404, statusText:"Not Found"}))
   );
